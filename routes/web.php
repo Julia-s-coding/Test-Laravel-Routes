04303ca139161004e038b3209c7cb725dc49752c
Julia-s-coding <juliaeifert@web.de>
2025-01-29 00:25:50 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::middleware('auth')->group( function() {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-
+    Route::prefix('admin')->group(function(){
 
         // Tasks inside that /admin group:
 
@@ -81,7 +81,7 @@ Route::middleware('auth')->group( function() {
         // Put one code line here below
 
 
-    // End of the /admin Route Group
+    });// End of the /admin Route Group
 
 });// End of the main Authenticated Route Group
 
